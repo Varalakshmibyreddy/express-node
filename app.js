a98b1c4ef6e17ab1030ad1d18b1a737cdce0cb78
Varalakshmibyreddy <varubyreddy@gmail.com>
2024-11-12 22:25:33 +0530

    added

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-
-const express = require("express") 
+const express = require("express")
 
 const app = express()
 
 const port = 5000
 
-app.get('/apple', (req, res)=>{
-    res.send("apple is red in color")
+app.get('/home', (req, res) =>{
+    res.send("Hello, i am home page")
+})
+
+app.get('/about', (req, res) =>{
+    res.send("this is About page")
+})
+
+app.get('user/:121', (req, res) =>{
+    res.send("you searched for 121")
 })
 
 app.listen(port,()=>{
- console.log("Server start and running succesfully")
+    console.log("Server Start and running Succesfully")
 })
-
